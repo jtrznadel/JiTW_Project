88131f00967d3cccba4bdf8e02b287256f732e41
jtrznadel <trznadeljakub3ai@gmail.com>
2023-01-09 23:39:02 +0100

    Updated

--- scripts.js
+++ scripts.js
@@ -6,7 +6,22 @@ const options = {
 	}
 };
 
-fetch('https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h', options)
+fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => createCoinsTable(response.data.coins))
 	.catch(err => console.error(err));
+
+function createCoinsTable(data){
+	const coinsTableBodyElement = document.getElementById("coinsTableBody");
+	data.forEach(element => {
+		const coinsTableRowElement  = document.createElement("tr");
+		 coinsTableRowElement.innerHTML = `
+		 <td>${element.rank}</td>
+		 <td><img src="${element.iconUrl}" width="24px" alt="${element.name} icon">${element.name}</td>
+		 <td>${element.symbol}</td>
+		 <td>${parseFloat((element.price)).toLocaleString("en-US", {style:"currency", currency:"USD"})}</td>
+		 <td>${element.marketCap}</td>
+		 `
+		coinsTableBodyElement.appendChild(coinsTableRowElement);
+	});
+};
